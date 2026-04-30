c95784a3757b33370b9459ca8bfd7a874ee765d0
lalitjoshi <lalit.joshi@programming.com>
2026-04-30 22:42:22 +0530

    Add GitHub Packages repository for dependency resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,12 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+        maven("https://maven.pkg.github.com/REPLACE_WITH_GITHUB_USERNAME/Carnival-SDK") {
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
+                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.key") as String?
+            }
+        }
     }
 }
 
